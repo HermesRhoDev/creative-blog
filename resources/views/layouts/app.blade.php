883854988d21e49c03720e01f4039dfcd10ee453
Guerrazzi Mathéo <materrazzi.pro@gmail.com>
2022-11-10 09:49:45 +0100

    adding styles css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,48 +12,47 @@
 <body>
 ​
     <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                <span class="fs-4">Blog Créative</span>
-            </a>
-​
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="/" class="nav-link active">Accueil</a></li>
-                {{-- <li class="nav-item"><a href="{{route('admin.dashboard')}}" class="nav-link active">Administration</a></li> --}}
-                @if (Route::has('login'))
-                    @auth
-                        <li class="nav-item">
-                            <a href="{{ url('/admin/') }}" class="nav-link">Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                    <a class="nav-link" href="route('logout')"
-                                            onclick="event.preventDefault();
-                                                        this.closest('form').submit();">
-                                        Déconnexion
-                                    </a>                                
-                            </form>
-                        </li>
-                    @else
-                        <a href="{{ route('login') }}" class="nav-link">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="nav-link">Register</a>
-                        @endif
-                    @endauth
-                @endif  
-            </ul>
-        </header>
-    </div>
-​
-    <div class="container" id="news" style="margin-bottom:150px">
-        {{-- @include('partials.alert') --}}
-        @yield('content')
-    </div>
-​
-    <div class="container">
-        <p>© 2022 Créative - Tous droits réservés</p>
+        <div class="mb-5">
+            <div class="container">
+                <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                        <span class="fw-semibold fs-4">Blog Créative</span>
+                    </a>
+                    <ul class="nav nav-pills">
+                        <li class="nav-item"><a href="/" class="nav-link active">Accueil</a></li>
+                        @if (Route::has('login'))
+                            @auth
+                                <li class="nav-item">
+                                    <a href="{{ url('/admin/') }}" class="nav-link">Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                            <a class="nav-link" href="route('logout')"
+                                                    onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                                Déconnexion
+                                            </a>                                
+                                    </form>
+                                </li>
+                            @else
+                                <a href="{{ route('login') }}" class="nav-link">Log in</a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="nav-link">Register</a>
+                                @endif
+                            @endauth
+                        @endif  
+                    </ul>
+                </header>
+            </div>
+            <div class="container" id="news" style="margin-bottom:150px">
+                {{-- @include('partials.alert') --}}
+                @yield('content')
+            </div>
+            <div class="container">
+                <p>© 2022 Créative - Tous droits réservés</p>
+            </div>
+        </div>
     </div>
     
 </body>
